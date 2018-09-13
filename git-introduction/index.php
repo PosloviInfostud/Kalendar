98d81d376e8a3b263084598f7a5e849c85b00554
chojan <chojan.co@gmail.com>
2018-09-13 14:06:20 +0200

    Add one more input

--- git-introduction/index.php
+++ git-introduction/index.php
@@ -35,7 +35,7 @@ if (empty($_GET['git_message'])) { ?>
         </div>
         <form action="" method="GET">
             <input type="text" name="git_message" id="git_message">
-            <input type="text" name="git_message1" id="git_message1">
+            <input type="text" name="git_message2" id="git_message2">
             <input type="submit" value="Pošalji" />
         </form>
     </div>
