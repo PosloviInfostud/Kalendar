2aae6a6ca9cd9806accd544af63b0a0596b23e87
LukaMatkovic <lukamatkovicns@gmail.com>
2018-09-25 14:19:08 +0200

    added login form

--- www/application/views/index.php
+++ www/application/views/index.php
@@ -1,7 +1,17 @@
 <div class="container mt-3">
     <div class="row">
         <div id="show" class="col-9">
-            
+            <form id="login_form" class="border border-primary rounded p-3">
+                <div class="form-group">
+                    <label for="email">E-Mail</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password" name="password" id="password">Password</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <input type="submit" class="btn btn-primary">
+            </form>
         </div>
         <div class="col-3">
             <button class="btn btn-info btn-block" id="register_button">Register</button>
