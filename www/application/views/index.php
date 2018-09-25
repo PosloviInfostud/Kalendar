48e785a3220b632edbc3db8f28469b03cf308d22
LukaMatkovic <lukamatkovicns@gmail.com>
2018-09-25 14:06:25 +0200

    added buttons and form structure

---
+++ www/application/views/index.php
@@ -0,0 +1,11 @@
+<div class="container mt-3">
+    <div class="row">
+        <div id="show" class="col-9">
+            
+        </div>
+        <div class="col-3">
+            <button class="btn btn-info btn-block" id="register_button">Register</button>
+            <button class="btn btn-primary btn-block" id="login_button">Login</button>
+        </div>
+    </div>
+</div>
