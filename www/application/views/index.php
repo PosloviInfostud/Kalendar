eb578e670a20c3c597e6bd2b2d698853526e3245
aAtila <alacanatila@gmail.com>
2018-09-25 14:21:10 +0200

    added register form

--- www/application/views/index.php
+++ www/application/views/index.php
@@ -1,7 +1,27 @@
 <div class="container mt-3">
     <div class="row">
         <div id="show" class="col-9">
-            
+
+<form id="register_form" class="border border-info rounded p-3">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Name</label>
+    <input type="text" class="form-control" name="name" id="name">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" name="email" id="email">
+  </div>
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Confirm Password</label>
+    <input type="password" class="form-control" id="password_confirm">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
         </div>
         <div class="col-3">
             <button class="btn btn-info btn-block" id="register_button">Register</button>
