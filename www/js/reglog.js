572b4fbae47d5fc53f4be0223c98af6cc4470c0a
LukaMatkovic <lukamatkovicns@gmail.com>
2018-09-25 14:48:11 +0200

    added css i jquery

---
+++ www/js/reglog.js
@@ -0,0 +1,8 @@
+$("#register_button").click(function(){
+    $("#login_form").addClass("hide");
+    $("#register_form").removeClass("hide");
+})
+$("#login_button").click(function(){
+    $("#register_form").addClass("hide");
+    $("#login_form").removeClass("hide");
+})
