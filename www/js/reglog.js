9b24dfd8e0bf9884e8f3ad836d52335a23237900
Atila Alacan <alacanatila@gmail.com>
2018-09-27 10:23:26 +0200

    Register ajax updated and changed append to html for the messages

--- www/js/reglog.js
+++ www/js/reglog.js
@@ -11,9 +11,9 @@ $("#login_button").click(function () {
 })
 
 // Register user
-$("#register_form").submit
-(function(e){
+$("#register_form").submit(function(e){
     e.preventDefault();
+    console.log("submitted");
     $.ajax({
         method: "POST",
         url: "/users/register",
@@ -25,6 +25,7 @@ $("#register_form").submit
         }
     })
     .done(function(response){
+        $("#messages").html(response);
     })
 })
 
@@ -40,6 +41,6 @@ $("#login_form").submit(function(e){
         }
     })
     .done(function(response){
-        $("#messages").append(response);
+        $("#messages").html(response);
     })
 })
