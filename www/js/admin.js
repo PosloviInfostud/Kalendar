61cd38c1bee2f1405fd2c043e4306dd0b2c24437
aAtila <alacanatila@gmail.com>
2018-10-03 14:47:52 +0200

    update user function

--- www/js/admin.js
+++ www/js/admin.js
@@ -13,3 +13,41 @@ $(".btn-options").click(function()
     });
     $(this).addClass('btn-info').removeClass('btn-outline-info');
 })
+
+// Load user edit view
+$(".user-edit").click(function()
+{
+    console.log($(this).attr("data-id"));
+    $.ajax({
+        method: "POST",
+        url: "/users/edit",
+        data: {
+            "user_id" : $(this).attr("data-id")
+        }
+    })
+    .done(function(response) {
+        $("#table").html(response);
+    });
+})
+
+// Update user
+$("#update_user").submit(function(e){
+    e.preventDefault();
+    $.ajax({
+        method: "POST",
+        url: "/users/update",
+        data: {
+            "name" : $("#user_name").val(),
+            "email" : $("#user_email").val(),
+            // "password" : $("#register_password").val(),
+            // "password_confirm" : $("#register_password_confirm").val()
+        }
+    })
+    .done(function(response){
+        if(response === 'success') {
+            window.location.href = "/users"
+        } else {
+            $("#messages").html(response);
+        }
+    })
+})
