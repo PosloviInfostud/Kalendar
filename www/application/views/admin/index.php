712e32f24f5cc33c4cd80e4d6344eb716f7afb7f
aAtila <alacanatila@gmail.com>
2018-10-03 16:28:35 +0200

    add new button for items

--- www/application/views/admin/index.php
+++ www/application/views/admin/index.php
@@ -2,6 +2,7 @@
     <h1 class="mb-5">Admin area</h1>
     <div class="row">
         <div class="col"><button type="button" class="btn btn-block btn-outline-info btn-options" data-name="reservations">Reservations</button></div>
+        <div class="col"><button type="button" class="btn btn-block btn-outline-info btn-options" data-name="items">Items</button></div>
         <div class="col"><button type="button" class="btn btn-block btn-outline-info btn-options" data-name="users">Users</button></div>
         <div class="col"><button type="button" class="btn btn-block btn-outline-info btn-options" data-name="user-activites">User Activites</button></div>
         <div class="col"><button type="button" class="btn btn-block btn-outline-info btn-options" data-name="logs">Logs</button></div>
