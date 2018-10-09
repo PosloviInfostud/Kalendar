5c566dcc890b0d221ef2bb9649b6ac6a6a368d8f
Atila Alacan <alacanatila@gmail.com>
2018-10-09 04:02:50 +0200

    update logs view

--- www/application/views/admin/logs.php
+++ www/application/views/admin/logs.php
@@ -1,27 +1,27 @@
 <h3 class="mt-5 mb-3">List of all logs</h3>
 
 <!-- Check if there are any entries in the db -->
-<?php if(empty($users)) {
+<?php if(empty($logs)) {
         echo 'No entries';
 } else { ?>
         <table class="table table-text-sm table-condensed table-striped border">
             <thead class="thead-light">
                 <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Role</th>
-                    <th scope="col">Active</th>
-                    <th scope="col">Registered @</th>
+                    <th scope="col">User</th>
+                    <th scope="col">Table</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Value</th>
+                    <th scope="col">Timestamp</th>
                 </tr>
             </thead>
             <tbody>
-            <?php foreach($users as $user) { ?> 
+            <?php foreach($logs as $log) { ?> 
                     <tr>
-                        <td class="align-middle text-center"><?= $user['name'] ?></td>
-                        <td class="align-middle text-center"><?= $user['email'] ?></td>
-                        <td class="align-middle text-center"><?= $user['role'] ?></td>
-                        <td class="align-middle text-center"><?= $user['active'] == 1 ? 'Yes' : 'No' ?></td>
-                        <td class="align-middle text-center"><?= $user['created_at'] ?></td>
+                        <td class="align-middle text-center"><?= $log['user_email'] ?></td>
+                        <td class="align-middle text-center"><?= $log['altered_table'] ?></td>
+                        <td class="align-middle text-center"><?= $log['type'] ?></td>
+                        <td class="align-middle"><?= $log['value'] ?></td>
+                        <td class="align-middle text-center"><?= $log['created_at'] ?></td>
                     </tr>
             <?php } ?>
             </tbody>
