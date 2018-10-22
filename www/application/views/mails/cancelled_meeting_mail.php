1eba556c2ca36e153e6da86e9d514914363452a4
LukaMatkovic <lukamatkovicns@gmail.com>
2018-10-22 11:08:51 +0200

    reg_log - fixed small errors

---
+++ www/application/views/mails/cancelled_meeting_mail.php
@@ -0,0 +1,4 @@
+<h2>Cancelled Meeting on <?= $start_time; ?></h2>
+<p>Dear <?=$name; ?>, <br>
+    We have a meeting in a <?= $title; ?> from <?= $start_time; ?> until <?= $end_time; ?>.<br>
+    See you!</p>
