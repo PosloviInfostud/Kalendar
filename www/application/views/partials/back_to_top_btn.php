f2c5e364f2ac776e853b0f19ff5099e6eaed9c7d
aAtila <alacanatila@gmail.com>
2018-11-15 14:20:33 +0100

    Make a partial from the 'Back to top' button

---
+++ www/application/views/partials/back_to_top_btn.php
@@ -0,0 +1,5 @@
+<div id="back_to_top_btn" class="hidden cursor-pointer fixed bg-primary hover:bg-primary-dark text-primary-lighter m-4 md:m-8 h-10 w-10 sm:h-12 sm:w-12 pin-b pin-r pin-4 z-50 opacity-50 rounded">
+    <div class="fill-current h-5 w-5 xs:h-6 xs:w-6">
+        <?= file_get_contents("public/icons/chevron-up.svg") ?>
+    </div>
+</div>
